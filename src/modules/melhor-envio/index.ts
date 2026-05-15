d8639b578850872e7fbf784410f795b72b104acb
FelipeBerro <berro.felipe@gmail.com>
2026-05-15 20:17:22 -0300

    Fix melhor-envio: export service directly, not wrapped in Module()

--- src/modules/melhor-envio/index.ts
+++ src/modules/melhor-envio/index.ts
@@ -1,4 +1,3 @@
-import { Module } from "@medusajs/framework/utils"
 import MelhorEnvioFulfillmentService from "./service"
 
-export default Module("melhor-envio", { service: MelhorEnvioFulfillmentService })
+export default MelhorEnvioFulfillmentService
