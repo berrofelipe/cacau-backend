407d42b68d97d14847dd5af81bd0dc75232cd353
FelipeBerro <berro.felipe@gmail.com>
2026-05-15 20:22:36 -0300

    Fix melhor-envio: use ModuleProvider instead of Module

--- src/modules/melhor-envio/index.ts
+++ src/modules/melhor-envio/index.ts
@@ -1,3 +1,6 @@
+import { ModuleProvider, Modules } from "@medusajs/framework/utils"
 import MelhorEnvioFulfillmentService from "./service"
 
-export default MelhorEnvioFulfillmentService
+export default ModuleProvider(Modules.FULFILLMENT, {
+  services: [MelhorEnvioFulfillmentService],
+})
